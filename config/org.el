1ad1a1e356ce6745af0c3acb4866282402ddfa4c
AledavR <150876244+AledavR@users.noreply.github.com>
2024-05-12 08:57:54 -0500

    Modified org-capture templates
    org-capture templates were modified to be build from variables, it helps with
    readability and modularity. Also the general template was modified to include a
    crated property to work better with Orgzly.

--- config/org.el
+++ config/org.el
@@ -18,67 +18,74 @@
   :elpaca nil
   :bind ("C-c c" . org-capture)
   :preface
+  (defvar my/org-agenda-file
+    "~/Dropbox/org/agenda.org")
+  (defvar my/org-ideas-file
+    "~/Dropbox/org/ideas.org")
+  (defvar my/org-created-property
+    "\n:PROPERTIES:\n:CREATED: [%<%Y-%m-%d %a %H:%M>]\n:END:")
+  (defvar my/org-file-link
+    "\n\nArchivo: [[%L][%f]]")
   (defvar my/org-idea-template
-    "* IDEA %^{Idea}\n\nCapturado %<%Y-%m-%d %H:%M>"
+    (concat "* IDEA %^{Idea}" my/org-created-property)
     "Plantilla básica de ideas.")
   (defvar my/org-idea-template-with-file
-    "* IDEA %^{Idea}\n\nCapturado %<%Y-%m-%d %H:%M>\nArchivo: [[%L][%f]]"
+    (concat my/org-idea-template my/org-file-link)
     "Plantilla de ideas con archivo.")
   (defvar my/org-basic-task-template
-    "* TODO %^{Tarea}\n\nCapturado %<%Y-%m-%d %H:%M>"
+    (concat "* TODO %^{Tarea}" my/org-created-property)
     "Plantilla básica de tareas.")
   (defvar my/org-basic-task-template-with-file
-    "* TODO %^{Tarea}\n\nCapturado %<%Y-%m-%d %H:%M>\nArchivo: [[%L][%f]]"
+    (concat my/org-basic-task-template my/org-file-link)
     "Plantilla básica de tareas con archivo.")
   (defvar my/org-deadline-task-template
-    "* TODO %^{Tarea}\nDEADLINE: %^t\n\nCapturado %<%Y-%m-%d %H:%M>"
+    (concat "* TODO %^{Tarea}\nDEADLINE: %^t" my/org-created-property)
     "Plantilla de tareas con fecha limite.")
   (defvar my/org-deadline-task-template-with-file
-    "* TODO %^{Tarea}\nDEADLINE: %^t
-\nCapturado %<%Y-%m-%d %H:%M>\nArchivo: [[%L][%f]]"
+    (concat my/org-deadline-task-template my/org-file-link)
     "Plantilla de tareas con fecha limite y archivo.")
   :custom
   (org-capture-templates
    `(
      ("t" "Tareas")
      ("tt" "Tarea de Universidad" entry
-      (file+headline "~/dropbox/org/agenda.org" "Universidad"),
+      (file+headline my/org-agenda-file "Universidad"),
       my/org-basic-task-template
       :empty-lines 1)
      ("tT" "Tarea de Universidad con archivo" entry
-      (file+headline "~/dropbox/org/agenda.org" "Universidad"),
+      (file+headline my/org-agenda-file "Universidad"),
       my/org-basic-task-template-with-file
       :empty-lines 1)
      ("tu" "Tarea de Universidad con fecha limite" entry
-      (file+headline "~/dropbox/org/agenda.org" "Universidad"),
+      (file+headline my/org-agenda-file "Universidad"),
       my/org-deadline-task-template
       :empty-lines 1)
      ("tU" "Tarea de Universidad con fecha limite y archivo" entry
-      (file+headline "~/dropbox/org/agenda.org" "Universidad"),
+      (file+headline my/org-agenda-file "Universidad"),
       my/org-deadline-task-template-with-file
       :empty-lines 1)
      ("tp" "Personal" entry
-      (file+headline "~/dropbox/org/agenda.org" "Personal"),
+      (file+headline my/org-agenda-file "Personal"),
       my/org-basic-task-template
       :empty-lines 1)
      ("tP" "Tarea personal con archivo" entry
-      (file+headline "~/dropbox/org/agenda.org" "Personal"),
+      (file+headline my/org-agenda-file "Personal"),
       my/org-basic-task-template-with-file
       :empty-lines 1)
      ("tl" "Personal con fecha limite" entry
-      (file+headline "~/dropbox/org/agenda.org" "Personal"),
+      (file+headline my/org-agenda-file "Personal"),
       my/org-deadline-task-template
       :empty-lines 1)
      ("tL" "Personal con fecha limite y archivo" entry
-      (file+headline "~/dropbox/org/agenda.org" "Personal"),
+      (file+headline my/org-agenda-file "Personal"),
       my/org-deadline-task-template-with-file
       :empty-lines 1)
      ("i" "Idea" entry
-      (file+headline "~/dropbox/org/ideas.org" "Ideas"),
+      (file+headline my/org-ideas-file "Ideas"),
       my/org-idea-template
       :empty-lines 1)
      ("I" "Idea con archivo" entry
-      (file+headline "~/dropbox/org/ideas.org" "Ideas"),
+      (file+headline my/org-ideas-file "Ideas"),
       my/org-idea-template-with-file
       :empty-lines 1)
      )))
